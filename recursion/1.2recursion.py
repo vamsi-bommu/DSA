385d15303542448962cc23eeef42306827ec404d
vamsi <vamsibommu64@gmail.com>
2026-03-10 07:48:27 +0530

    feat: Add recursive implementations for array swapping, palindrome checking, and Fibonacci sequence calculation, along with an iterative Fibonacci solution.

--- recursion/1.2recursion.py
+++ recursion/1.2recursion.py
@@ -24,3 +24,26 @@ s="dummud"
 print(palindrome(s,l=0,r=len(s)-1))
 TC=O(N)  #N/2   AS we go till middle
 SC=O(N)  #N/2   AS we go till middle
+
+
+# #Nth fibonacci 
+m,n=0,1
+N=7
+for i in range(N-2):
+    t=m+n
+    m=n
+    n=t
+
+print(n)
+TC=O(N)
+SC=O(1)
+
+#Nth fibonacci using recursion
+def fib(n):
+    if n<=1:
+        return n
+    return fib(n-1)+fib(n-2)
+
+print(fib(8))
+TC=O(2**N)  #each call makes 2 recursive calls
+SC=O(N)     #
