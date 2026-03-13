87ed36cd5cfbf83405410740a4e4126000251bef
vamsi <vamsibommu64@gmail.com>
2026-03-13 19:55:21 +0530

    feat: add recursive functions to print all subsequences and subsequences with a target sum.

--- recursion/subsequence.py
+++ recursion/subsequence.py
@@ -1,4 +1,4 @@
-## Print all subsequences
+# Print all subsequences
 
 def subsequence(n,a,arr):
     if n>=len(arr):
@@ -11,4 +11,20 @@ def subsequence(n,a,arr):
     subsequence(n+1,a,arr) #not take
     
 subsequence(0,[],arr=[3,1,2,7])
+
+#print subsequences whose sum is k
+
+def subseq(i,a,arr,sum,s):
+
+    if i>=len(arr):
+        if sum==s:
+            print(a)
+        return
+
+    a.append(arr[i])
+    subseq(i+1,a,arr,sum+arr[i],s)
+    a.pop()
+    subseq(i+1,a,arr,sum,s)
+
+subseq(0,[],[1,2,1,1],0,2)
 
