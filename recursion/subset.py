707ef9c684aa409b5939875fb379ef548e1edb61
Bommu vamsi <vamsibommu64@gmail.com>
2026-04-14 11:18:22 +0530

    Refactor subset functions for clarity and consistency

--- recursion/subset.py
+++ recursion/subset.py
@@ -1,18 +1,41 @@
-# subset sums
+# subset sums 
+
 # Given an array arr of integers, return the sums of all subsets in the list.  Return the sums in any order
 
-
-def comb(i, a, arr, result):
+def subset_sums(i, a, arr, result):
     if i >= len(arr):
         result.append(a)
         return
 
     a += arr[i]
-    comb(i + 1, a, arr, result)
+    subset_sums(i + 1, a, arr, result)
     a -= arr[i]
-    comb(i + 1, a, arr, result)
+    subset_sums(i + 1, a, arr, result)
 
 
 result = []
-comb(0, 0, [1, 2, 1], result)
+subset_sums(0, 0, [1, 2, 1], result)
 print(result)
+
+
+# subset sum II
+
+#Given an integer array nums that may contain duplicates, return all possible subsets (the power set) ,The solution set must not contain duplicate subsets.
+
+def subset_sum_II(i, a, arr, result):
+    
+    result.append(a.copy())
+
+    for j in range(i, len(arr)):
+        if j != i and arr[j] == arr[j - 1]:
+            continue
+        a.append(arr[j])
+        print(j + 1, a)
+        subset_sum_II(j + 1, a, arr, result)
+        a.pop()
+
+
+result = []
+subset_sum_II(0, [], sorted(nums), result)
+return (result)
+
