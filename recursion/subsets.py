c5361c68d284a8f24b3e6d3a46c2f1025659dc8c
vamsi <vamsibommu64@gmail.com>
2026-04-17 08:03:17 +0530

    feat: implement recursive algorithms for subset sums and unique subsets

--- recursion/subsets.py
+++ recursion/subsets.py
@@ -1,6 +1,7 @@
-# subset sums 
+# subset sums
 
 # Given an array arr of integers, return the sums of all subsets in the list.  Return the sums in any order
+
 
 def subset_sums(i, a, arr, result):
     if i >= len(arr):
@@ -17,30 +18,30 @@ result = []
 subset_sums(0, 0, [1, 2, 1], result)
 print(result)
 
-#TC :
-#SC :
+# TC :
+# SC :
 
 
 # subset sum II
 
-#Given an integer array nums that may contain duplicates, return all possible subsets (the power set) ,The solution set must not contain duplicate subsets.
+# Given an integer array nums that may contain duplicates, return all possible subsets (the power set) ,The solution set must not contain duplicate subsets.
+
 
 def subset_sum_II(i, a, arr, result):
-    
+
     result.append(a.copy())
 
     for j in range(i, len(arr)):
         if j != i and arr[j] == arr[j - 1]:
             continue
         a.append(arr[j])
-        print(j + 1, a)
         subset_sum_II(j + 1, a, arr, result)
         a.pop()
 
 
 result = []
-subset_sum_II(0, [], sorted(nums), result)
+subset_sum_II(0, [], sorted([1, 2, 2, 2, 3, 3]), result)
 print(result)
 
-#TC :
-#SC :
+# TC :
+# SC :
