56dc66c2891e8832d9e30a2e37ce0a54b7e3d410
Bommu vamsi <vamsibommu64@gmail.com>
2026-04-27 13:39:48 +0530

    Create sudoko_solver.py

---
+++ recursion/sudoko_solver.py
@@ -0,0 +1,18 @@
+# +-------+-------+-------+
+# | 5 3 . | . 7 . | . . . |
+# | 6 . . | 1 9 5 | . . . |
+# | . 9 8 | . . . | . 6 . |
+# +-------+-------+-------+
+# | 8 . . | . 6 . | . . 3 |
+# | 4 . . | 8 . 3 | . . 1 |
+# | 7 . . | . 2 . | . . 6 |
+# +-------+-------+-------+
+# | . 6 . | . . . | 2 8 . |
+# | . . . | 4 1 9 | . . 5 |
+# | . . . | . 8 . | . 7 9 |
+# +-------+-------+-------+
+
+# Conditions:
+  # Each of the digits 1-9 must occur exactly once in each row.
+  # Each of the digits 1-9 must occur exactly once in each column.
+  # Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
